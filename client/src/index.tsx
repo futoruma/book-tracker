6c18ce484540fbc6d0df4b2fe49123e5dd315e76
futoruma <122431649+futoruma@users.noreply.github.com>
2024-09-30 21:51:47 +0900

    add client paths

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
+import { Paths } from './paths';
+import { Login } from './pages/login';
+import { Signup } from './pages/signup';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Book Tracker</h1>
+  },
+  {
+    path: Paths.login,
+    element: <Login />
+  },
+  {
+    path: Paths.signup,
+    element: <Signup />
+  }
+])
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -11,7 +30,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>Hello World!</h1>
+      <RouterProvider router={ router } />
     </Provider>
   </React.StrictMode>
 );
---
+++ client/src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+    home: "/",
+    bookAdd: "/book/add",
+    bookEdit: "/book/edit",
+    book: "/book",
+    status: "/status",
+    login: "/login",
+    signup: "/signup"
+} as const;
