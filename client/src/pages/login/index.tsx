6879ded7ab737a177b8ae74f37559a1d5e69185c
futoruma <122431649+futoruma@users.noreply.github.com>
2024-10-02 21:42:46 +0900

    add custom inputs

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,9 +1,14 @@
-import { Layout } from "../../components/layout"
+import { Card, Form, Row } from "antd";
+import { Layout } from "../../components/layout";
 
 export const Login = () => {
-    return (
-        <Layout>
-            <div>Login</div>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Enter" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}></Form>
+        </Card>
+      </Row>
+    </Layout>
+  );
+};
