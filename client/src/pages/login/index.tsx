a6e1a0104141523f5879c65469a4f3a2d6771895
futoruma <122431649+futoruma@users.noreply.github.com>
2024-10-02 23:32:03 +0900

    add signup form

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -10,7 +10,7 @@ export const Login = () => {
   return (
     <Layout>
       <Row align="middle" justify="center">
-        <Card title="Enter" style={{ width: "30rem" }}>
+        <Card title="Sign in" style={{ width: "30rem" }}>
           <Form onFinish={() => null}>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Password" />
--- client/src/pages/signup/index.tsx
+++ client/src/pages/signup/index.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { Link } from "react-router-dom";
+import { Layout } from "../../components/layout";
+import { CustomInput } from "../../components/custom-input";
+import { PasswordInput } from "../../components/password-input";
+import { CustomButton } from "../../components/custom-button";
+import { Paths } from "../../paths";
 
 export const Signup = () => {
-    return (
-        <div>Sign up</div>
-    )
-}
+  return (
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Sign up" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput name="name" placeholder="Name" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Password" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Confirm password"
+            />
+            <CustomButton type="primary" htmlType="submit">
+              Submit
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Already have an account? <Link to={Paths.login}>Log in here</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
+  );
+};
