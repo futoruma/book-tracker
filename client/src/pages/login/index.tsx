410fd72abe33dd5ad6fb04c56b64e53404bcc514
futoruma <122431649+futoruma@users.noreply.github.com>
2024-10-03 23:52:06 +0900

    add error message

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -5,13 +5,33 @@ import { CustomInput } from "../../components/custom-input";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
 import { Paths } from "../../paths";
+import { useLoginMutation, UserData } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { useState } from "react";
+import { ErrorMessage } from "../../components/error-message";
 
 export const Login = () => {
+  const [loginUser, loginUserResult] = useLoginMutation();
+  const [error, setError] = useState("");
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap();
+    } catch (error) {
+      const maybeError = isErrorWithMessage(error);
+      if (maybeError) {
+        setError(error.data.message);
+      } else {
+        setError("Unknown error");
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Sign in" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={login}>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Password" />
             <CustomButton type="primary" htmlType="submit">
@@ -22,6 +42,7 @@ export const Login = () => {
             <Typography.Text>
               No account yet? <Link to={Paths.signup}>Sign up here</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
