acf048ebe223750ab7a6de82d858e275f911f191
futoruma <122431649+futoruma@users.noreply.github.com>
2024-10-14 02:22:45 +0900

    add signup

--- client/src/pages/signup/index.tsx
+++ client/src/pages/signup/index.tsx
@@ -1,17 +1,44 @@
 import { Card, Form, Row, Space, Typography } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Layout } from "../../components/layout";
 import { CustomInput } from "../../components/custom-input";
 import { PasswordInput } from "../../components/password-input";
 import { CustomButton } from "../../components/custom-button";
 import { Paths } from "../../paths";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/auth/authSlice";
+import { useState } from "react";
+import { useSignupMutation } from "../../app/services/auth";
+import { User } from "@prisma/client";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { ErrorMessage } from "../../components/error-message";
+
+type SignupData = Omit<User, "id"> & { confirmPassword: string };
 
 export const Signup = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [error, setError] = useState("");
+  const [signupUser] = useSignupMutation();
+
+  const signup = async (data: SignupData) => {
+    try {
+      await signupUser(data).unwrap();
+      navigate("/");
+    } catch (error) {
+      const maybeError = isErrorWithMessage(error);
+      if (maybeError) {
+        setError(error.data.message);
+      } else {
+        setError("Unknown error");
+      }
+    }
+  };
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Sign up" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={signup}>
             <CustomInput name="name" placeholder="Name" />
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Password" />
@@ -27,6 +54,7 @@ export const Signup = () => {
             <Typography.Text>
               Already have an account? <Link to={Paths.login}>Log in here</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
