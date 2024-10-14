d4c4acc6763beaf9df151276bdc54823aa829ea4
futoruma <122431649+futoruma@users.noreply.github.com>
2024-10-14 20:42:23 +0900

    fix header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import {
   LoginOutlined,
   LogoutOutlined,
-  TeamOutlined,
+  BookOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import { Layout as AntdLayout, Space, Typography } from "antd";
@@ -25,35 +25,23 @@ export const Header = () => {
 
   return (
     <AntdLayout.Header className={styles.header}>
-      <Space>
-        <TeamOutlined className={styles.teamIcon} />
-        <Link to={Paths.home}>
-          <CustomButton type="primary">
-            <Typography.Title level={3}>Books</Typography.Title>
-          </CustomButton>
-        </Link>
-      </Space>
       {user ? (
-        <CustomButton
-          type="primary"
-          icon={<LogoutOutlined />}
-          onClick={onLogoutClick}
-        >
-          Logout
-        </CustomButton>
+        <>
+          <Space>
+            <Link to={Paths.home}>
+              <BookOutlined className={styles.teamIcon} />
+            </Link>
+          </Space>
+          <CustomButton
+            type="primary"
+            icon={<LogoutOutlined />}
+            onClick={onLogoutClick}
+          >
+            Logout
+          </CustomButton>
+        </>
       ) : (
-        <Space>
-          <Link to={Paths.signup}>
-            <CustomButton type="primary" icon={<UserOutlined />}>
-              Sign Up
-            </CustomButton>
-          </Link>
-          <Link to={Paths.login}>
-            <CustomButton type="primary" icon={<LoginOutlined />}>
-              Login
-            </CustomButton>
-          </Link>
-        </Space>
+        <Space></Space>
       )}
     </AntdLayout.Header>
   );
